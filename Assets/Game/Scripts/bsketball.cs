c6bcddec71b2337c4af5c0b8c3221616d95456d3
lhz-heihei <2433924799@qq.com>
2023-07-02 10:53:20 +0800

    add state of run when normal,repair problem that ball backward when dribbing,delete the sportgate beneath basketball frame

--- Assets/Game/Scripts/bsketball.cs
+++ Assets/Game/Scripts/bsketball.cs
@@ -6,13 +6,17 @@ public class bsketball : MonoBehaviour
 {
     public GameObject righthand;
     float timer = 1.16f;
-    Vector3 dir = new Vector3(0, 0.03f, -0.02f);
+    Vector3 dir = new Vector3(0, 0.06f, -0.02f);
     public GameObject camera1;
     public Animator animator;
     public bool isdrib = false;
     public bool isshoot = false;
     Vector3 down = new Vector3(0, 1f, 0);
     public ShootBasketball _shootbasketball;
+    public float bounceSpeed = 2f;
+    public float bounceMagnitude = 2f;
+    public long smoothSpeed = 1;
+    public float targetBounceOffset;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,61 +33,27 @@ public class bsketball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isdrib)
-        {
-            timer += Time.deltaTime;
-            if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.D)))
-            {
-                if (timer >= 0.3f)
-                {
-                    gameObject.transform.position = righthand.transform.position - dir;
-                    timer = 0;
-                }
-                if (timer >= 0.15f && timer <= 0.3f)
-                {
-                    //gameObject.transform.LookAt(righthand.transform.position);
-                    gameObject.transform.Translate(Vector3.up * Time.deltaTime * 5f);
-                }
-                if (timer <= 0.15f)
-                {
-                    gameObject.transform.Translate(Vector3.down * 5f * Time.deltaTime);
-                }
-            }
-            else
-            {
-                if (timer >= 0.3f)
-                {
-                    gameObject.transform.position = righthand.transform.position - dir;
-                    timer = 0;
-                }
-                if (timer >= 0.15f && timer <= 0.3f)
-                {
-                    //gameObject.transform.LookAt(righthand.transform.position);
-                    gameObject.transform.Translate(Vector3.up * Time.deltaTime * 3f);
-                }
-                if (timer <= 0.15f)
-                {
-                    gameObject.transform.Translate(Vector3.down * 3f * Time.deltaTime);
-                }
-            }
-        }
         
-        /*if (Input.GetKey(KeyCode.W))
+        if (isdrib)
         {
-            //transform.Translate(Vector3.up * 3f*Time.deltaTime);
-            transform.Translate(camera1.transform.up* 3f * Time.deltaTime);
+            Vector3 targetPosition = righthand.transform.position - dir;
+            Vector3 desiredPosition = new Vector3(targetPosition.x, targetBounceOffset, targetPosition.z);
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            timer += Time.deltaTime;
+            float deylaspeed = targetPosition.y/0.25f;
+            if (timer >= 0.4f)
+            {
+                gameObject.transform.position = righthand.transform.position - dir;
+                timer = 0;
+            }
+            if (timer >= 0.2f && timer <= 0.4f)
+            {
+                targetBounceOffset = deylaspeed * (timer-0.2f);
+            }
+            if (timer <= 0.2f)
+            {
+                targetBounceOffset = targetPosition.y - deylaspeed * timer;
+            }
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * 2f*Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * 2f*Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * 2f*Time.deltaTime);
-        }*/
     }
 }
